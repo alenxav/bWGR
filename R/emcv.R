8cc7baf03812811b789d0a685deb56ab8e967a9d
Alencar Xavier <33139566+alenxav@users.noreply.github.com>
2018-07-12 09:45:40 -0500

    Update emcv.R

--- R/emcv.R
+++ R/emcv.R
@@ -1,41 +1,37 @@
-emCV=function(y, gen, k = 5, n = 5){
-  # Cross-validation function
-  folds = function(Seed,y,gen,k,it,bi){
+emCV = function (y, gen, k = 5, n = 5, Pi = 0.75, alpha = 0.02, df = 10, R2 = 0.5) {
+  folds = function(Seed, y, gen, k) {
     N = nrow(gen)
-    # Begin folds
     set.seed(Seed)
     Nk = round(N/k)
-    w=sample(1:N,Nk)
-    Y=y; y[w]=NA
-    f1=emRR(y[-w],gen[-w,])
-    f2=emEN(y[-w],gen[-w,])
-    f3=emBL(y[-w],gen[-w,])
-    f4=emDE(y[-w],gen[-w,])
-    f5=emBA(y[-w],gen[-w,])
-    f6=emBB(y[-w],gen[-w,])
-    f7=emBC(y[-w],gen[-w,])
-    cat('DONE WITH CROSS-VALIDATION CYCLE',Seed,'\n')
-    NamesMod = c('emRR','emEN','emBL','emDE',
-                 'emBA','emBB','emBC','OBSERVATION')
-    M = matrix(NA,Nk,length(NamesMod))
+    w = sample(1:N, Nk)
+    Y = y
+    y[w] = NA
+    f1 = emRR(y[-w], gen[-w, ], R2 = R2, df = df)
+    f2 = emEN(y[-w], gen[-w, ], alpha = alpha, R2 = R2)
+    f3 = emBL(y[-w], gen[-w, ], alpha = alpha, R2 = R2)
+    f4 = emDE(y[-w], gen[-w, ], R2 = R2)
+    f5 = emBA(y[-w], gen[-w, ], R2 = R2, df = df)
+    f6 = emBB(y[-w], gen[-w, ], Pi = Pi, R2 = R2, df = df)
+    f7 = emBC(y[-w], gen[-w, ], Pi = Pi, R2 = R2, df = df)
+    cat("DONE WITH CROSS-VALIDATION CYCLE", Seed, "\n")
+    NamesMod = c("emRR", "emEN", "emBL", "emDE", "emBA", 
+                 "emBB", "emBC", "OBSERVATION")
+    M = matrix(NA, Nk, length(NamesMod))
     colnames(M) = NamesMod
-    for(i in 1:7) M[,i]=gen[w,]%*%get(paste('f',i,sep=''))$b
-    M[,8] = Y[w]
+    for (i in 1:7) M[, i] = gen[w, ] %*% get(paste("f", i,sep = ""))$b
+    M[, 8] = Y[w]
     return(M)
   }
-  # Running cross-validations
-  Seeds=1:n
-  b = lapply(Seeds,FUN=folds,y=y,gen=gen,k=k)
-  names(b) = paste('CV_',1:length(b),sep='')
-  # Summary function
-  sCV=function(cv){
+  Seeds = 1:n
+  b = lapply(Seeds, FUN = folds, y = y, gen = gen, k = k)
+  names(b) = paste("CV_", 1:length(b), sep = "")
+  sCV = function(cv) {
     n = length(cv)
     m = ncol(cv$CV_1)
-    dta = matrix(0,0,m)
-    for(i in 1:n) dta = rbind(dta,cv[[i]])
-    # Summary
-    PA = sort(cor(dta)[-m,m],decreasing = TRUE)
-    return(round(PA,digits=4))
+    dta = matrix(0, 0, m)
+    for (i in 1:n) dta = rbind(dta, cv[[i]])
+    PA = sort(cor(dta)[-m, m], decreasing = TRUE)
+    return(round(PA, digits = 4))
   }
   return(sCV(b))
 }
