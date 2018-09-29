88d8ece5eef0491dbdc3b14567aa0ce29e3dfea7
Alencar Xavier <33139566+alenxav@users.noreply.github.com>
2018-09-29 13:49:49 -0500

    Update cv.R

--- R/cv.R
+++ R/cv.R
@@ -1,4 +1,4 @@
-emCV = function (y, gen, k=5, n=5, Pi=0.75, alpha=0.02, df=10, R2=0.5, avg=TRUE) {
+emCV = function (y, gen, k=5, n=5, Pi=0.75, alpha=0.02, df=10, R2=0.5, avg=TRUE,llo=NULL){
   folds = function(Seed, y, gen, k) {
     N = nrow(gen)
     set.seed(Seed)
@@ -23,10 +23,37 @@ emCV = function (y, gen, k=5, n=5, Pi=0.75, alpha=0.02, df=10, R2=0.5, avg=TRUE)
     M[,length(NamesMod)] = Y[w]
     return(M)
   }
-  Seeds = 1:n
-  b = lapply(Seeds, FUN = folds, y = y, gen = gen, k = k)
+  llo_folds = function(lev, y, gen) {
+    w = which(llo==lev)
+    Nk = length(w)
+    Y = y
+    y[w] = NA
+    f1 = emRR(y[-w], gen[-w, ], R2 = R2, df = df)
+    f2 = emEN(y[-w], gen[-w, ], alpha = alpha, R2 = R2)
+    f3 = emBL(y[-w], gen[-w, ], alpha = alpha, R2 = R2)
+    f4 = emDE(y[-w], gen[-w, ], R2 = R2)
+    f5 = emBA(y[-w], gen[-w, ], R2 = R2, df = df)
+    f6 = emBB(y[-w], gen[-w, ], Pi = Pi, R2 = R2, df = df)
+    f7 = emBC(y[-w], gen[-w, ], Pi = Pi, R2 = R2, df = df)
+    f8 = emML(y[-w], gen[-w, ])
+    cat("DONE WITH CROSS-VALIDATION CYCLE", lev, "\n")
+    NamesMod = c("emRR", "emEN", "emBL", "emDE", "emBA", 
+                 "emBB", "emBC", "emML", "OBSERVATION")
+    M = matrix(NA, Nk, length(NamesMod))
+    colnames(M) = NamesMod
+    for (i in 1:(length(NamesMod)-1)) M[, i] = gen[w, ] %*% get(paste("f", i,sep = ""))$b
+    M[,length(NamesMod)] = Y[w]
+    return(M)
+  }
+  if(is.null(llo)){
+    Seeds = 1:n
+    b = lapply(Seeds, FUN = folds, y = y, gen = gen, k = k)
+  }else{
+    lev = unique(as.character(llo))
+    b = lapply(lev, FUN = llo_folds, y = y, gen = gen)
+  }
   names(b) = paste("CV_", 1:length(b), sep = "")
-    sCV = function(cv) {
+  sCV = function(cv) {
     n = length(cv)
     m = ncol(cv$CV_1)
     if(avg){
@@ -44,7 +71,7 @@ emCV = function (y, gen, k=5, n=5, Pi=0.75, alpha=0.02, df=10, R2=0.5, avg=TRUE)
   return(sCV(b))
 }
 
-mcmcCV = function (y, gen, k = 5, n = 5, it=1500, bi=500, pi=0.95, df=5, R2=0.5, avg=TRUE) {
+mcmcCV = function (y, gen, k = 5, n = 5, it=1500, bi=500, pi=0.95, df=5, R2=0.5, avg=TRUE,llo=NULL){
   folds = function(Seed, y, gen, k) {
     N = nrow(gen)
     set.seed(Seed)
@@ -61,17 +88,44 @@ mcmcCV = function (y, gen, k = 5, n = 5, it=1500, bi=500, pi=0.95, df=5, R2=0.5,
     f7 = BayesDpi(y[-w], gen[-w, ], R2 = R2, df = df, it=it, bi=bi)
     cat("DONE WITH CROSS-VALIDATION CYCLE", Seed, "\n")
     NamesMod = c("BayesA", "BayesB", "BayesC", "BayesL",
-                  "BayesCpi", "BayesDpi", "BayesRR", "OBSERVATION")
+                 "BayesCpi", "BayesDpi", "BayesRR", "OBSERVATION")
     M = matrix(NA, Nk, length(NamesMod))
     colnames(M) = NamesMod
     for (i in 1:(length(NamesMod)-1)) M[, i] = gen[w, ] %*% get(paste("f", i,sep = ""))$b
     M[, length(NamesMod)] = Y[w]
     return(M)
   }
-  Seeds = 1:n
-  b = lapply(Seeds, FUN = folds, y = y, gen = gen, k = k)
+  llo_folds = function(lev, y, gen) {
+    w = which(llo==lev)
+    Nk = length(w)
+    Y = y
+    y[w] = NA
+    f1 = BayesA(y[-w], gen[-w, ], R2 = R2, df = df, it=it, bi=bi)
+    f2 = BayesB(y[-w], gen[-w, ], R2 = R2, df = df, it=it, bi=bi, pi=pi)
+    f3 = BayesC(y[-w], gen[-w, ], R2 = R2, df = df, it=it, bi=bi, pi=pi)
+    f4 = BayesL(y[-w], gen[-w, ], R2 = R2, df = df, it=it, bi=bi)
+    f5 = BayesRR(y[-w], gen[-w, ], R2 = R2, df = df, it=it, bi=bi)
+    f6 = BayesCpi(y[-w], gen[-w, ], R2 = R2, df = df, it=it, bi=bi)
+    f7 = BayesDpi(y[-w], gen[-w, ], R2 = R2, df = df, it=it, bi=bi)
+    cat("DONE WITH CROSS-VALIDATION CYCLE", lev, "\n")
+    NamesMod = c("BayesA", "BayesB", "BayesC", "BayesL",
+                 "BayesCpi", "BayesDpi", "BayesRR", "OBSERVATION")
+    M = matrix(NA, Nk, length(NamesMod))
+    colnames(M) = NamesMod
+    for (i in 1:(length(NamesMod)-1)) M[, i] = gen[w, ] %*% get(paste("f", i,sep = ""))$b
+    M[, length(NamesMod)] = Y[w]
+    return(M)
+  }
+  if(is.null(llo)){
+    Seeds = 1:n
+    b = lapply(Seeds, FUN = folds, y = y, gen = gen, k = k)
+  }else{
+    lev = unique(as.character(llo))
+    b = lapply(lev, FUN = llo_folds, y = y, gen = gen)
+  }
+  
   names(b) = paste("CV_", 1:length(b), sep = "")
-    sCV = function(cv) {
+  sCV = function(cv) {
     n = length(cv)
     m = ncol(cv$CV_1)
     if(avg){
