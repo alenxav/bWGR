cf51e508e9c842627ace31c284a9447c5ed31e21
Alencar Xavier <33139566+alenxav@users.noreply.github.com>
2018-09-29 15:40:01 -0500

    Update wgr20180916.cpp

--- src/wgr20180916.cpp
+++ src/wgr20180916.cpp
@@ -1388,15 +1388,28 @@ NumericMatrix GAU(NumericMatrix X){
     for(int i=0; i<n; i++){K(i,_) = exp(-K(i,_)/md);} return K;}
 
 // [[Rcpp::export]]
+#include <Rcpp.h>
+using namespace Rcpp;
+// [[Rcpp::export]]
 NumericMatrix GRM(NumericMatrix X, bool Code012 = false){
   int n = X.nrow(), p = X.ncol();
   NumericMatrix K(n,n); NumericVector xx(p); double zz, Sum2pq=0.0;
   for(int i=0; i<p; i++){ xx[i] = mean(X(_,i)); }
-  if(Code012){for(int i=0; i<p; i++){ Sum2pq = Sum2pq + xx[i]*xx[i]/2;}
-  }else{ for(int i=0; i<p; i++){ Sum2pq = Sum2pq + var(X(_,i));}}
-  for(int i=0; i<n; i++){; for(int j=0; j<n; j++){; if(i<=j ){
-   zz = sum( (X(i,_)-xx(i))*(X(j,_)-xx(j)) );
-   K(i,j)=zz; K(j,i)=zz;}}}; return K/Sum2pq;}
+  if(Code012){
+    for(int i=0; i<p; i++){ Sum2pq = Sum2pq + xx[i]*xx[i]/2;}
+  }else{
+    for(int i=0; i<p; i++){Sum2pq = Sum2pq + var(X(_,i));}
+  }
+  for(int i=0; i<n; i++){
+    for(int j=0; j<n; j++){
+      if(i<=j ){
+        zz = sum( (X(i,_)-xx)*(X(j,_)-xx) );
+        K(i,j)=zz; K(j,i)=zz;
+      }
+    }
+  }
+  return K/Sum2pq;
+}
 
 // [[Rcpp::export]]
 NumericVector SPC(NumericVector y, NumericVector blk, NumericVector row, NumericVector col, int rN=3, int cN=1){
