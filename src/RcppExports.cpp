c56de1c138a3f43cb9b7923440da640b7f176ba2
Alencar Xavier <33139566+alenxav@users.noreply.github.com>
2019-06-05 10:09:07 -0500

    Add files via upload

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -435,27 +435,15 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
-// mrrV2
-SEXP mrrV2(NumericMatrix Y, NumericMatrix X);
-RcppExport SEXP _bWGR_mrrV2(SEXP YSEXP, SEXP XSEXP) {
+// mrrR
+SEXP mrrR(NumericMatrix Y, NumericMatrix X);
+RcppExport SEXP _bWGR_mrrR(SEXP YSEXP, SEXP XSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
     Rcpp::traits::input_parameter< NumericMatrix >::type Y(YSEXP);
     Rcpp::traits::input_parameter< NumericMatrix >::type X(XSEXP);
-    rcpp_result_gen = Rcpp::wrap(mrrV2(Y, X));
-    return rcpp_result_gen;
-END_RCPP
-}
-// mrrV3
-SEXP mrrV3(NumericMatrix Y, NumericMatrix X);
-RcppExport SEXP _bWGR_mrrV3(SEXP YSEXP, SEXP XSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< NumericMatrix >::type Y(YSEXP);
-    Rcpp::traits::input_parameter< NumericMatrix >::type X(XSEXP);
-    rcpp_result_gen = Rcpp::wrap(mrrV3(Y, X));
+    rcpp_result_gen = Rcpp::wrap(mrrR(Y, X));
     return rcpp_result_gen;
 END_RCPP
 }
@@ -545,8 +533,7 @@ static const R_CallMethodDef CallEntries[] = {
     {"_bWGR_SPC", (DL_FUNC) &_bWGR_SPC, 6},
     {"_bWGR_SPM", (DL_FUNC) &_bWGR_SPM, 5},
     {"_bWGR_mrr", (DL_FUNC) &_bWGR_mrr, 2},
-    {"_bWGR_mrrV2", (DL_FUNC) &_bWGR_mrrV2, 2},
-    {"_bWGR_mrrV3", (DL_FUNC) &_bWGR_mrrV3, 2},
+    {"_bWGR_mrrR", (DL_FUNC) &_bWGR_mrrR, 2},
     {"_bWGR_mrr2X", (DL_FUNC) &_bWGR_mrr2X, 3},
     {"_bWGR_mtgsru", (DL_FUNC) &_bWGR_mtgsru, 7},
     {"_bWGR_mkr", (DL_FUNC) &_bWGR_mkr, 2},
--- src/wgr20190605.cpp
+++ src/wgr20190605.cpp
@@ -1511,100 +1511,6 @@ NumericMatrix SPM(NumericVector blk, NumericVector row, NumericVector col, doubl
 
 // [[Rcpp::export]]
 SEXP mrr(NumericMatrix Y, NumericMatrix X){
-  // Convergence criteria
-  int maxit = 200;
-  double tol = 10e-8;
-  // Obtain environment containing function
-  Rcpp::Environment base("package:base");
-  Rcpp::Function solve = base["solve"];
-  // Functions starts here
-  int k = Y.ncol(), p = X.ncol(), n0 = X.nrow();
-  NumericMatrix fit(n0,k),o(n0,k),y(n0,k),eps(n0,k),e(n0,k);
-  for(int i=0; i<k; i++){
-    o(_,i) = ifelse(is_na(Y(_,i)),0,1);
-    y(_,i) = ifelse(is_na(Y(_,i)),0,Y(_,i));}
-  NumericVector n = colSums(o);
-  // Mu
-  NumericVector mu = colSums(y)/n, mu0(k);
-  for(int j=0; j<k; j++){eps(_,j) = (y(_,j)-mu(j))*o(_,j);}
-  // Marker variance
-  NumericMatrix xx(p,k), vx(p,k);
-  double tmp;
-  for(int i=0; i<p; i++){
-    for(int j=0; j<k; j++){
-      xx(i,j) = sum(X(_,i)*X(_,i)*o(_,j));
-      tmp = sum(X(_,i)*o(_,j))/n(j);
-      vx(i,j) = xx(i,j)/n(j)-tmp*tmp;}}
-  NumericVector MSx = colSums(vx);
-  // Beta, intersept and residuals
-  NumericMatrix b(p,k),vb(k,k),rho(k,k),LHS(k,k);
-  NumericVector b0(k),b1(k),vy(k),ve(k),RHS(k);
-  for(int i=0; i<k; i++){for(int j=0; j<k; j++){vb(i,j) = 0;}}
-  for(int i=0; i<k; i++){
-    e(_,i) = y(_,i)+0;
-    vy(i) = sum(e(_,i)*e(_,i))/(n(i)-1);
-    ve(i) = vy(i)*0.5;
-    vb(i,i) = ve(i)/MSx(i);
-    rho(i,i) = 1;}
-  NumericMatrix iG = solve(vb);
-  // Convergence control
-  NumericMatrix bc(p,k);
-  int numit = 0;
-  double cnv = 1;
-  // Loop
-  while(numit<maxit){
-    // Gauss-Seidel loop
-    bc = b+0;
-    for(int j=0; j<p; j++){
-      b0 = b(j,_);
-      LHS = iG+0;
-      for(int i=0; i<k; i++){
-        LHS(i,i) = iG(i,i)+(xx(j,i)/ve(i));
-        RHS(i) = (sum(e(_,i)*X(_,j))+xx(j,i)*b0(i))/ve(i);}
-      // Update effects
-      b1 = solve(LHS, RHS);
-      b(j,_) = b1;
-      // Update residuals
-      for(int i=0; i<k; i++){
-        e(_,i) = (e(_,i)-X(_,j)*(b1(i)-b0(i)))*o(_,i);}
-    }
-    // Update mu and epsilon
-    mu0 = colSums(e)/n;
-    mu = mu+mu0;
-    for(int j=0; j<k; j++){eps(_,j) = (y(_,j)-mu(j))*o(_,j);}
-    // Residual variance components update
-    for(int i=0; i<k; i++){ ve(i) = (sum(e(_,i)*y(_,i)))/n(i);}
-    // Genetic covariance components update
-    for(int i=0; i<n0; i++){ for(int j=0; j<k; j++){fit(i,j) = sum(X(i,_)*b(_,j));}}
-    for(int i=0; i<k; i++){ for(int j=0; j<k; j++){
-      vb(i,j) = (sum(fit(_,i)*eps(_,j))+sum(fit(_,j)*eps(_,i)))/((n(i)*MSx(i))+(n(j)*MSx(j)));}}
-    // Ridge and inverse of G
-    for(int i=0; i<k; i++){ vb(i,i)=vb(i,i)*1.01;}
-    iG = solve(vb);
-    // Convergence
-    ++numit;
-    cnv = sum(abs(bc-b));
-    if( cnv<tol ){break;}}
-  // Remove ridging from genetic variance
-  for(int i=0; i<k; i++){ vb(i,i)=vb(i,i)/1.01;}
-  // Fitting the model
-  for(int i=0; i<n0; i++){for(int j=0; j<k; j++){fit(i,j)=sum(X(i,_)*b(_,j))+mu(j);}}
-  // Heritability
-  NumericVector h2 = 1-ve/vy;
-  // Genetic correlations
-  NumericMatrix GC(k,k);
-  for(int i=0; i<k; i++){ for(int j=0; j<k; j++){GC(i,j)=vb(i,j)/(sqrt(vb(i,i)*vb(j,j)));}}
-  // Output
-  return List::create(Named("mu")=mu,
-                      Named("b")=b,
-                      Named("hat")=fit,
-                      Named("h2")=h2,
-                      Named("Vb")=vb,
-                      Named("Ve")=ve,
-                      Named("GC")=GC);}
-
-// [[Rcpp::export]]
-SEXP mrrV2(NumericMatrix Y, NumericMatrix X){
   int maxit = 200;
   double tol = 10e-6;
   // Obtain environment containing function
@@ -1663,14 +1569,13 @@ SEXP mrrV2(NumericMatrix Y, NumericMatrix X){
         e(_,i) = (e(_,i)-X(_,j)*(b1(i)-b0(i)))*o(_,i);}
     }
     // Residual variance components update
-    for(int i=0; i<k; i++){ ve(i) = (sum(e(_,i)*y(_,i)))/(n(i)-1);}
+    for(int i=0; i<k; i++){ ve(i) = (sum(e(_,i)*y(_,i)))/n(i);}
     // Genetic covariance components update
     for(int i=0; i<n0; i++){ for(int j=0; j<k; j++){fit(i,j) = sum(X(i,_)*b(_,j));}}
     for(int i=0; i<k; i++){ for(int j=0; j<k; j++){
       // Diag VC
       if(i==j){
-        vb(i,j) = (1.01*vy(i)-ve(i))/MSx(i);
-      }else{
+        vb(i,j) = (1.01*vy(i)-ve(i))/MSx(i); }else{
         vb(i,j) = (sum(fit(_,i)*y(_,j))+sum(fit(_,j)*y(_,i))) / ((n(i)*MSx(i))+(n(j)*MSx(j)));
       }}}
     iG = solve(vb);
@@ -1679,7 +1584,8 @@ SEXP mrrV2(NumericMatrix Y, NumericMatrix X){
     cnv = sum(abs(bc-b));
     if( cnv<tol ){break;}}
   // Fitting the model
-  NumericVector h2(k); 
+  NumericVector h2(k);
+  for(int i=0; i<k; i++){ vb(i,i) = sum(fit(_,i)*y(_,i))/(n(i)*MSx(i)); }
   for(int i=0; i<n0; i++){for(int j=0; j<k; j++){fit(i,j) = sum(X(i,_)*b(_,j))+mu(j);}}
   h2 = 1-ve/vy;
   // Genetic correlations
@@ -1692,7 +1598,7 @@ SEXP mrrV2(NumericMatrix Y, NumericMatrix X){
                       Named("Ve")=ve);}
 
 // [[Rcpp::export]]
-SEXP mrrV3(NumericMatrix Y, NumericMatrix X){
+SEXP mrrR(NumericMatrix Y, NumericMatrix X){
   // Convergence criteria
   int maxit = 200;
   double tol = 10e-8;
