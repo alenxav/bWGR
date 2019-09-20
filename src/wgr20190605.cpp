9b2391900221f999e5e8d7a45751d39f589d0d16
Alencar Xavier <33139566+alenxav@users.noreply.github.com>
2019-09-20 13:30:51 -0500

    Update wgr20190605.cpp

--- src/wgr20190605.cpp
+++ src/wgr20190605.cpp
@@ -257,6 +257,65 @@ SEXP emBC(NumericVector y, NumericMatrix gen, double df = 10, double R2 = 0.5, d
 }
 
 // [[Rcpp::export]]
+SEXP emBD(NumericVector y, NumericMatrix gen, double df = 10, double R2 = 0.5){
+  int it = 200;
+  int p = gen.ncol();
+  int n = gen.nrow();
+  double ve = 1;
+  NumericVector d(p);
+  NumericVector b(p);
+  NumericVector vb = b+1;
+  NumericVector Lmb = ve/vb;
+  double vy = var(y);
+  double Pi = 0.50;
+  NumericVector xx(p);
+  NumericVector vx(p);
+  for(int i=0; i<p; i++){
+    xx[i] = sum(gen(_,i)*gen(_,i));
+    vx[i] = var(gen(_,i));}
+  double MSx = sum(vx)*Pi;
+  double Sb = R2*(df+2)*vy/MSx;
+  double Se = (1-R2)*(df+2)*vy;
+  double mu = mean(y);
+  NumericVector e = y-mu;
+  NumericVector e1(n);
+  NumericVector e2(n);
+  double b0,b1,LR,eM,h2,C;
+  double Pi0 = (1-Pi)/Pi;
+  for(int i=0; i<it; i++){
+    C = -0.5/ve;
+    for(int j=0; j<p; j++){
+      b0 = b[j];
+      b1 = (sum(gen(_,j)*e)+xx[j]*b0)/(xx[j]+Lmb[j]);
+      e1 = e-gen(_,j)*(b1-b0);
+      e2 = e-gen(_,j)*(0-b0);
+      LR = Pi0*exp(C*(sum(e2*e2)-sum(e1*e1)));
+      d[j] = (1/(1+LR));
+      b[j] = b1*d[j];
+      vb[j] = (Sb+b[j]*b[j])/(df+1);
+      e = e - gen(_,j)*(b[j]-b0);}
+    ve = (sum(e*e)+Se)/(n+df);
+    R2 = 1-(sum(e*y)/(n-1))/vy;
+    Pi = mean(d);
+    Sb = R2*(df+2)*vy/MSx/Pi;
+    Se = (1-R2)*(df+2)*vy;
+    Lmb = ve/vb;
+    eM = mean(e);
+    mu = mu+eM;
+    e = e-eM;}
+  h2 = 1-ve/vy;
+  NumericVector fit(n);
+  for(int k=0; k<n; k++){fit[k] = sum(gen(k,_)*b)+mu;}
+  return List::create(Named("mu") = mu,
+                      Named("b") = b,
+                      Named("d") = d,
+                      Named("pi") = 1-Pi,
+                      Named("hat") = fit,
+                      Named("Vb") = vb,
+                      Named("Ve") = ve,
+                      Named("h2") = h2);}
+
+// [[Rcpp::export]]
 SEXP emRR(NumericVector y, NumericMatrix gen, double df = 10, double R2 = 0.5){
   int it = 200;
   int p = gen.ncol();
