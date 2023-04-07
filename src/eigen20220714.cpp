4c535a3f3e29afd11aa231470b9ed1369c13b797
Alencar Xavier <33139566+alenxav@users.noreply.github.com>
2023-04-06 19:33:37 -0500

    Update eigen20220714.cpp

--- src/eigen20220714.cpp
+++ src/eigen20220714.cpp
@@ -4,24 +4,22 @@
 
 // [[Rcpp::export]]
 Eigen::MatrixXd EigenARC(Eigen::MatrixXd X, bool centralizeZ = true, int cores = 2){
+   // cseweb.ucsd.edu/~saul/papers/nips09_kernel.pdf
   Eigen::setNbThreads(cores); int p = X.cols(), n = X.rows(); 
-  double tmp, Npi=3.1416;
-  if(centralizeZ){
+  double tmp, Npi=3.1416, theta, J1, Kij, Norm;
+  if(centralizeX){
     for(int i=0; i<p; i++){
       tmp = (X.col(i).array()).mean();
       X.col(i) = X.col(i).array()-tmp;}}
   Eigen::MatrixXd XXp = X*X.transpose();
   tmp = 1/(XXp.diagonal().mean()); XXp *= tmp;
   Eigen::VectorXd DiagXXp = XXp.diagonal().array();
-  Eigen::VectorXd SqDiagXXp = DiagXXp.array().sqrt();
-  for(int i=0; i<n; i++){
-    for(int j=0; j<n; j++){
-      if(i>j){
-        tmp = acos( XXp(i,j)/(SqDiagXXp(i)*SqDiagXXp(j)) );
-        //tmp = (sin(tmp)*DiagXXp[i]*DiagXXp[j])/Npi+(Npi-tmp)*cos(tmp);
-        tmp = (sin(tmp)*DiagXXp[i]*DiagXXp[j])+(Npi-tmp)*cos(tmp);
-        tmp /= Npi; XXp(i,j) = tmp*1.0; XXp(j,i) = tmp*1.0;
-      }else if(i==j){ XXp(i,j) = 1;}}}
+  for(int i=0; i<n; i++){ for(int j=0; j<n; j++){ if(i>=j){ 
+    Norm = sqrt(DiagXXp(i)*DiagXXp(j));
+    theta = acos( XXp(i,j)/Norm);
+    J1 = sin(theta) + (Npi-theta)*cos(theta);
+    Kij = Norm/Npi*J1;
+    XXp(i,j) = Kij*1.0; XXp(j,i) = Kij*1.0;}}}
   return XXp;}
 
 // [[Rcpp::export]]
