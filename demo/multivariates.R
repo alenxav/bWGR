a98b1110a1749cb805d5850b76555de328261440
Alencar Xavier <33139566+alenxav@users.noreply.github.com>
2024-06-06 21:20:10 -0500

    Add files via upload

--- demo/multivariates.R
+++ demo/multivariates.R
@@ -1,20 +1,17 @@
-require(bWGR)
-Z = SimZ(ind = 1000)
-GC = SimGC(MIX = F)
-S = SimY(Z = Z, GC = GC)
+Z = SimZ()
+GC = SimGC()
+S = SimY(Z=Z, GC=GC)
 Y = S$Y
 tbv = S$tbv
-fit_mega = MEGAF(Y,Z)$gebv
-fit_gsem = GSEMF(Y,Z)$hat
 
-fit_uv = Z %*% FUVBETA(Y,Z)
-fit_mrr = MRR3F(Y,Z)$hat
-fit_xfa = MRR3(Y,Z,XFA=T)$hat
-fit_sem = SEM(Y,Z)$hat
-fit_xsem = XSEMF(Y,Z)$hat
-fit_zsem = ZSEMF(Y,Z)$hat
-fit_gsem = GSEMF(Y,Z)$hat
-fit_mega = MEGAF(Y,Z)$gebv
+system.time(fit_mega <- MEGA(Y,Z)$gebv)[3]
+system.time(fit_gsem <- GSEM(Y,Z)$hat)[3]
+system.time(fit_fuvbeta <- Z %*% FUVBETA(Y,Z))[3]
+system.time(fit_mrrf <- MRR3F(Y,Z)$hat)[3]
+system.time(fit_xfa <- MRR3(Y,Z,XFA<-T)$hat)[3]
+system.time(fit_sem <- SEM(Y,Z)$hat)[3]
+system.time(fit_xsemf <- XSEMF(Y,Z)$hat)[3]
+system.time(fit_zsemf <- ZSEMF(Y,Z)$hat)[3]
 
-models = grep('fit',ls(),value=T) 
-sort(sapply(models, function(x) mean(diag(cor(get(x),tbv)))))
+sort(sapply(grep('fit',ls(),value=T),
+            function(x) mean(diag(cor(get(x),tbv)))))
