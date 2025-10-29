eade21c563f20b1e959ef1fae72bc99154c5bc54
Alencar Xavier <33139566+alenxav@users.noreply.github.com>
2025-10-29 17:29:44 -0500

    Enhance mixed model analysis with data simulation
    Added data simulation and reshaping functionality, and updated model comparison and plotting methods.

--- demo/mixedmodel.R
+++ demo/mixedmodel.R
@@ -1,26 +1,31 @@
-# Load data
-data(met,package='NAM')
-Obs$Block = factor(Obs$Block)
+# Simulate data
+Z = SimZ(ind = 1250)
+S = SimY(Z,GC = 0.75,k = 10, h2=0.25,PercMiss=0.5)
+Y = S$Y
+colnames(Z) = paste0("SNP", 1:ncol(Z))
+rownames(Z) = rownames(Y) = paste0("IND", 1:nrow(Z))
+colnames(Y) = paste0("ENV", 1:ncol(Y))
+dta = data.frame(GEN = paste0("IND", 1:nrow(Z)),Y,row.names = NULL)
 
-# Subset family 05
-fam = substr(rownames(Gen),6,7)
-Gen = Gen[which(fam%in%c('05')),]
-Gen = data.matrix(Gen-1)
-fam = substr(as.character(Obs$ID),6,7)
-Obs = droplevels.data.frame(Obs[which(fam=='05'),])
-rm(fam)
+# Reshape dta from wide to long data frame using native R function reshape
+dta = reshape(dta,varying = list(colnames(Y)),direction = 'long',v.names = 'YIELD',timevar = 'ENV',times = colnames(Y),idvar = 'GEN')
+dta$ENV = factor(dta$ENV)
+dta$GEN = factor(dta$GEN)
 
 # Compare BLUP vs gBLUP
-test1 = mixed(y=YLD,random=~Block+ID,fixed=~Year,data=Obs,X=list(ID=Gen))
-test2 = mixed(y=YLD,random=~Block+ID,fixed=~Year,data=Obs,maxit = 20)
-cor(test1$Coefficients$ID,test2$Coefficients$ID)
+test1 = mm(y=YIELD,random=~GEN+GEN*ENV,fixed=~ENV,data=dta,M=list(GEN=Z))
+test2 = mm(y=YIELD,random=~GEN,fixed=~ENV,data=dta)
+cor(test1$Coef$GEN,test2$Coef$GEN)
 
 # Plot
 par(mfrow=c(1,3))
-plot(test1$Fitness$hat,test1$Fitness$obs,main='Goodness of fit',xlab='Fitted',ylab='Observed',pch=20)
-abline(lm(test1$Fitness$obs~test1$Fitness$hat),col=2)
-legend('bottom',paste('Cor =',round(cor(test1$Fitness$hat,test1$Fitness$obs),4)))
-plot(test1$Coefficients$ID,test2$Coefficients$ID,xlab='GEBV',ylab='BLUP',pch=20,main='Genomic fit')
-abline(lm(test2$Coefficients$ID~test1$Coefficients$ID),col=2)
-legend('bottom',paste('Cor =',round(cor(test1$Coefficients$ID,test2$Coefficients$ID),4)))
-plot(test1$Structure$ID,main='Marker Effects',ylab='Effect',xlab='SNP',pch=20)
+plot(test1$GOF$Predicted,test1$GOF$Observed,main='Goodness of fit',xlab='Fitted',ylab='Observed',pch=20)
+abline(lm(test1$GOF$Observed~test1$GOF$Predicted),col=2)
+legend('bottom',paste('Cor =',round(cor(test1$GOF$Observed,test1$GOF$Predicted,use='p'),2)),bty='n')
+
+plot(test1$Coef$GEN,test2$Coef$GEN,xlab='GEBV',ylab='BLUP',pch=20,main='Genomic fit')
+abline(lm(test2$Coef$GEN~test1$Coef$GEN),col=2)
+legend('bottom',paste('Cor =',round(cor(test1$Coef$GEN,test2$Coef$GEN),2)),bty='n')
+
+plot(test1$Mrk$GEN,main='Marker Effects',ylab='Effect',xlab='SNP',pch=20,type='h')
+abline(h=0,col=2,lwd=3)
